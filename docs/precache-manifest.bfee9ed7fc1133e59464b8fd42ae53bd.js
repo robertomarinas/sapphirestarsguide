25b740243611d967642cc05de57063b0307e4399
Bobby Marinas <robertomartin.marinas@gmail.com>
2019-03-18 21:55:35 +0800

    updated github pages

---
+++ docs/precache-manifest.bfee9ed7fc1133e59464b8fd42ae53bd.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "45cebd9f9d1b279c11065b2a94fcaac1",
+    "url": "/sapphirestarsguide/static/media/longsword.45cebd9f.svg"
+  },
+  {
+    "revision": "7e750aa77629e614a25c",
+    "url": "/sapphirestarsguide/static/css/main.23aa8269.chunk.css"
+  },
+  {
+    "revision": "3d57f8c87267abc438c0",
+    "url": "/sapphirestarsguide/static/js/runtime~main.3d57f8c8.js"
+  },
+  {
+    "revision": "a15bdb5029e21c36ba50",
+    "url": "/sapphirestarsguide/static/js/2.a15bdb50.chunk.js"
+  },
+  {
+    "revision": "467e94933e9beeb340777b60e26c46cb",
+    "url": "/sapphirestarsguide/static/media/helm.467e9493.svg"
+  },
+  {
+    "revision": "fa1d1d98c76f0a30426baf00ed314bca",
+    "url": "/sapphirestarsguide/static/media/chest.fa1d1d98.svg"
+  },
+  {
+    "revision": "88fbc1e75af64450d71be5d0ab982ec2",
+    "url": "/sapphirestarsguide/static/media/arms.88fbc1e7.svg"
+  },
+  {
+    "revision": "b4c929bd76eba1e10a9bffaa35ada938",
+    "url": "/sapphirestarsguide/static/media/legs.b4c929bd.svg"
+  },
+  {
+    "revision": "59048cb99bc5ee91d3af2ad267888bef",
+    "url": "/sapphirestarsguide/static/media/weapon.59048cb9.svg"
+  },
+  {
+    "revision": "eb2e0bf012023c92469be343297e5263",
+    "url": "/sapphirestarsguide/static/media/swordandshield.eb2e0bf0.svg"
+  },
+  {
+    "revision": "d81e7d9ea30269b7b224412f0f908a6b",
+    "url": "/sapphirestarsguide/static/media/waist.d81e7d9e.svg"
+  },
+  {
+    "revision": "469ed23643bbf4f2d58cc0057c82aa5d",
+    "url": "/sapphirestarsguide/static/media/greatsword.469ed236.svg"
+  },
+  {
+    "revision": "96bae2ea48e8d8bdedc2c14001c57f91",
+    "url": "/sapphirestarsguide/static/media/dualblades.96bae2ea.svg"
+  },
+  {
+    "revision": "cb0ce4f106cada2a339e70674db22c79",
+    "url": "/sapphirestarsguide/static/media/charm.cb0ce4f1.svg"
+  },
+  {
+    "revision": "7e750aa77629e614a25c",
+    "url": "/sapphirestarsguide/static/js/main.7e750aa7.chunk.js"
+  },
+  {
+    "revision": "d0548dc67455577d903bde00993d7a80",
+    "url": "/sapphirestarsguide/static/media/chargeblade.d0548dc6.svg"
+  },
+  {
+    "revision": "361769fe31005823d610d1fb70621e6b",
+    "url": "/sapphirestarsguide/static/media/switchaxe.361769fe.svg"
+  },
+  {
+    "revision": "294e24630c8fd0b5ac9ca1833894fcc1",
+    "url": "/sapphirestarsguide/static/media/hammer.294e2463.svg"
+  },
+  {
+    "revision": "ddff2598c7afcd2457541d0924fe7aba",
+    "url": "/sapphirestarsguide/static/media/huntinghorn.ddff2598.svg"
+  },
+  {
+    "revision": "b701bad7ebea078a7bcad0418c96ca3b",
+    "url": "/sapphirestarsguide/static/media/lance.b701bad7.svg"
+  },
+  {
+    "revision": "e9bcc34a14e73059a7cf72b7de78cddc",
+    "url": "/sapphirestarsguide/static/media/gunlance.e9bcc34a.svg"
+  },
+  {
+    "revision": "3b10ca1dceaef5db32b02d672f1bf5bb",
+    "url": "/sapphirestarsguide/static/media/bow.3b10ca1d.svg"
+  },
+  {
+    "revision": "c24861a5c545fd470a651ae8e01e635b",
+    "url": "/sapphirestarsguide/static/media/heavybowgun.c24861a5.svg"
+  },
+  {
+    "revision": "dc443b4369c2e7004d7328fa46ba19ee",
+    "url": "/sapphirestarsguide/static/media/insectglaive.dc443b43.svg"
+  },
+  {
+    "revision": "1067b24e861a48db97dd35478baa7936",
+    "url": "/sapphirestarsguide/static/media/remove.1067b24e.svg"
+  },
+  {
+    "revision": "761ca4c4f0d4c6b6702b59046ab0c8a0",
+    "url": "/sapphirestarsguide/static/media/lightbowgun.761ca4c4.svg"
+  },
+  {
+    "revision": "17ec29c8a2c227f528ef12252dec7a3e",
+    "url": "/sapphirestarsguide/static/media/sapphirestar.17ec29c8.svg"
+  },
+  {
+    "revision": "b801ef53ea54ff2937b4428d7ea919aa",
+    "url": "/sapphirestarsguide/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7491e52ef0e0ddd3936f04681d6dca6a.js"
+  "/sapphirestarsguide/precache-manifest.bfee9ed7fc1133e59464b8fd42ae53bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sapphirestarsguide/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
